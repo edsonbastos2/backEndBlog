4e51b52dcccd4ba2f0bf12db9c14c5aa2ef1dd2d
Edson Bastos <edsonbastos2@hotmail.com>
2021-09-09 22:46:05 -0300

    Criado crud Posts

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,7 +1,36 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class PostsController {
-  public index() {
-    return ['post1', 'post2', 'post3']
+  public async index({}: HttpContextContract) {
+    const post = await Post.all()
+    return post
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = await Post.create(data)
+    return post
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    return post
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+
+    post.merge(data)
+
+    await post.save()
+
+    return post
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    await post.delete()
   }
 }
